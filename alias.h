f8ff3ca14256aea61684ab57e388c19182c1998e
McKing <97905873+4mcking@users.noreply.github.com>
2022-08-10 21:31:44 +0100

    Create alias.h

---
+++ alias.h
@@ -0,0 +1,20 @@
+#ifndef ALIAS_H
+#define ALIAS_H
+
+/**
+ * struct AliasData - struct for alias function
+ * @AliasData - name of the struct
+ * @name: name of the alias value
+ * @val: value for the alias
+ * @next: pointer to next node
+ *
+ */
+
+typedef struct AliasData
+{
+	char *name;
+	char *val;
+	struct AliasData *next;
+} AliasData;
+
+#endif
